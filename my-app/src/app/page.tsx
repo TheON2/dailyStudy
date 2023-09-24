859bedc7318ff25428d61e1a53d9f63a2e260585
TheON2 <sniperad@naver.com>
2023-09-24 09:47:43 +0900

    Update: complete chapter4

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,8 +1,6 @@
-import { Inter } from "next/font/google";
 import Image from "next/image";
-
-const inter = Inter({ subsets: ["latin"] });
+import styles from "./page.module.css";
 
 export default function Home() {
-  return <h1>홈페이지</h1>;
+  return <h1>홈페이지다!!</h1>;
 }
