50f5dd2b96f2ff9ab53705387320ed54edf56466
TheON2 <sniperad@naver.com>
2023-10-06 17:35:42 +0900

    Update : my-app

--- algorithm/solution.js
+++ algorithm/solution.js
@@ -1,6 +1,11 @@
-function solution() {
-
-  return ;
+function solution(num_list, n) {
+  let result = []
+  for (let i = n-1; i < num_list.length; i++) {
+    result.push(num_list[i])
+  }
+  return result
 }
 
-console.log(solution());
+console.log(
+  solution([2, 1, 6]	,3)
+);
